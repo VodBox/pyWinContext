19387ddfa94a1bcab491cf9f322d7f79abef0107
VodBox <dillon@vodbox.io>
2018-02-26 01:36:24 +1300

    Add Various UI Tweaks and Test Data

--- wincontext.py
+++ wincontext.py
@@ -3,7 +3,12 @@ import regutils as reg
 import time
 import re
 
+import ctypes
+myappid = 'VodBox.pyWinContext.1.0' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
 import sys
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTreeWidgetItem
 import app
 
@@ -14,6 +19,13 @@ class ExampleApp(QMainWindow, app.Ui_MainWindow):
 		self.initUI()
 
 	def initUI(self):
+		app_icon = QtGui.QIcon()
+		app_icon.addFile('images/icon_16.png', QtCore.QSize(16,16))
+		app_icon.addFile('images/icon_24.png', QtCore.QSize(24,24))
+		app_icon.addFile('images/icon_32.png', QtCore.QSize(32,32))
+		app_icon.addFile('images/icon_48.png', QtCore.QSize(48,48))
+		app_icon.addFile('images/icon.png', QtCore.QSize(256,256))
+		self.setWindowIcon(app_icon)
 		self.statusBar().showMessage('Ready')
 		self.setWindowTitle('pyWinContext')
 		fts = reg.get_file_types()
@@ -44,17 +56,38 @@ class ExampleApp(QMainWindow, app.Ui_MainWindow):
 		increment = 0
 		for type in types:
 			item_0 = QTreeWidgetItem(self.treeWidget_2)
+			item_0.setCheckState(0, QtCore.Qt.Unchecked)
+			item_0.numEnabled = 0
 			self.treeWidget_2.topLevelItem(increment).setText(0, type)
 			self.treeWidget_2.topLevelItem(increment).setText(1, "")
 			fileIncrement = 0
 			for file in types[type]:
 				item_1 = QTreeWidgetItem(item_0)
+				item_1.setCheckState(0, QtCore.Qt.Unchecked)
 				self.treeWidget_2.topLevelItem(increment).child(fileIncrement).setText(0, file["filetype"])
 				self.treeWidget_2.topLevelItem(increment).child(fileIncrement).setText(1, file["content-type"])
 				fileIncrement += 1
 			increment += 1
+		self.treeWidget.setSortingEnabled(False)
+		itemGroup = QTreeWidgetItem(self.treeWidget)
+		itemGroup.setBackground(0, QtGui.QColor(176, 234, 253))
+		itemGroup.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
+		itemGroup = QTreeWidgetItem(self.treeWidget)
+		itemGroup.setBackground(0, QtGui.QColor(176, 234, 253))
+		itemGroup.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
+		itemGroup = QTreeWidgetItem(self.treeWidget)
+		itemGroup.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDragEnabled)
+		self.treeWidget.topLevelItem(0).setText(0, "Group 1")
+		self.treeWidget.topLevelItem(0).setText(1, "Initial Group")
+		self.treeWidget.topLevelItem(1).setText(0, "Group 2")
+		self.treeWidget.topLevelItem(1).setText(1, "Initial Group")
+		self.treeWidget.topLevelItem(2).setText(0, "Command 1")
+		self.treeWidget.topLevelItem(2).setText(1, "My First Command")
 		self.treeWidget_2.setSortingEnabled(True)
 		self.treeWidget_2.sortItems(0, 0)
+		self.treeWidget.sortItems(0, 0)
+		self.treeWidget_2.itemChanged.connect(self.files_change)
+		self.treeWidget_2.resizeColumnToContents(0)
 		self.lineEdit_4.textChanged.connect(self.search_change)
 		self.show()
 
@@ -66,6 +99,31 @@ class ExampleApp(QMainWindow, app.Ui_MainWindow):
 					self.treeWidget_2.topLevelItem(i).child(x).setHidden(True)
 				else:
 					self.treeWidget_2.topLevelItem(i).child(x).setHidden(False)
+
+	def files_change(self, data):
+		parent = data.parent()
+		if data.childCount() > 0 and data.checkState(0) != QtCore.Qt.PartiallyChecked:
+			checkState = data.checkState(0)
+			for childIdx in range(0, data.childCount()):
+				oldState = data.treeWidget().blockSignals(True)
+				data.child(childIdx).setCheckState(0, checkState)
+				data.treeWidget().blockSignals(oldState)
+		elif parent != None and type(parent) is QTreeWidgetItem:
+			oldState = data.treeWidget().blockSignals(True)
+
+			numEnabled = 0
+			for childIdx in range(0, parent.childCount()):
+				if data.isSelected() and parent.child(childIdx) != data and parent.child(childIdx).isSelected():
+					parent.child(childIdx).setCheckState(0, data.checkState(0))
+				numEnabled += (1 if parent.child(childIdx).checkState(0) == QtCore.Qt.Checked else 0)
+			if numEnabled == parent.childCount():
+				parent.setCheckState(0, QtCore.Qt.Checked)
+			elif numEnabled > 0:
+				parent.setCheckState(0, QtCore.Qt.PartiallyChecked)
+			else:
+				parent.setCheckState(0, QtCore.Qt.Unchecked)
+
+			data.treeWidget().blockSignals(oldState)
 
 
 def main():
